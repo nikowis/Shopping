b93241f32198778069b6b913f76ffb5f926c7c23
nikowis <nikowis@gmail.com>
2017-03-18 22:05:24 +0100

    Added a static image to every list item.

--- app/src/main/java/pl/nikowis/shopping/ShoppingViewHolder.java
+++ app/src/main/java/pl/nikowis/shopping/ShoppingViewHolder.java
@@ -2,6 +2,7 @@ package pl.nikowis.shopping;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -11,10 +12,12 @@ public class ShoppingViewHolder extends RecyclerView.ViewHolder {
 
     protected TextView mTitleView;
     protected TextView mDescriptionView;
+    protected ImageView imageView;
 
     public ShoppingViewHolder(View itemView) {
         super(itemView);
         this.mTitleView = (TextView) itemView.findViewById(R.id.item_title);
         this.mDescriptionView = (TextView) itemView.findViewById(R.id.item_description);
+        this.imageView = (ImageView) itemView.findViewById(R.id.item_image);
     }
 }
