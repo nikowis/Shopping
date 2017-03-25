2e1da6ad7c757b71127de802018a26314e2cf70d
nikowis <nikowis@gmail.com>
2017-03-25 14:56:19 +0100

    Added a splash screen to the application.

---
+++ app/src/main/java/pl/nikowis/shopping/ui/SplashActivity.java
@@ -0,0 +1,32 @@
+package pl.nikowis.shopping.ui;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+import pl.nikowis.shopping.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
